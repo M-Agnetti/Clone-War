bc60a0d2f2df859d65760d9a27b80807942b15e7
Soumeya <soumia.saidi@edu.univ-eiffel.fr>
2022-10-10 09:44:35 +0200

    Webserver commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.WebServer;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args){
 
         System.out.println("hello");
-        
+        WebServer webServer = WebServer
+                .create(Routing.builder()
+                        .any((req, res) -> res.send("it works !!!")))
+                .start()
+                .await(10, TimeUnit.SECONDS);
+
+        System.out.println("Server started at: http://localhost:" + webServer.port());
     }
 }
